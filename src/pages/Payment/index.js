9b42c32b565b6d9e58285dcde07ffb97aa00bd04
Dominic Oludare <dominicoludare@gmail.com>
2021-12-05 02:47:57 +0100

    stripe update

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PaymentDetails from './../../components/PaymentDetails'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import { publishableKey } from './../../stripe/config';
-//import { usePaystackPayment } from 'react-paystack'
-//import { publickey } from './../../paystack/config';
+
+import { usePaystackPayment } from 'react-paystack'
+import { publickey } from './../../paystack/config';
 
 const Payment = () => {
 
-    const stripePromise = loadStripe(publishableKey);
+   // const stripePromise = loadStripe(publishableKey);
     return (
-        <Elements stripe={stripePromise}>
             <PaymentDetails />
-        </Elements>
     );
 }
 
