a798e6a67ce6bf6b8df80089be89208b95c820a4
Dominic Oludare <dominicoludare@gmail.com>
2021-12-05 02:53:41 +0100

    firebase update

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDSbOwnTRgK6vR0E3bVsLrMK9yomaiyk3g",
+    authDomain: "doggypup-ef08e.firebaseapp.com",
+    databaseURL: "https://doggypup-ef08e.firebaseio.com",
+    projectId: "doggypup-ef08e",
+    storageBucket: "doggypup-ef08e.appspot.com",
+    messagingSenderId: "861508086742",
+    appId: "1:861508086742:web:a74a5334513dfdd57e9277"
+  };
